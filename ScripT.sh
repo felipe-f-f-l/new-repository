cece44eb133fc1a569f638d2f69a9065bcef481c
felipe-f-f-l <61243576+felipe-f-f-l@users.noreply.github.com>
2020-03-05 10:40:13 -0300

    Add files via upload

---
+++ ScripT.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+unzip imagens-livros.zip
+cd imagens-livros
+mkdir imagens-png
+convert *.jpg *.png
+mv *.png imagens-png
+zip -r imagens-png.zip imagens-png/
+
+novo
+#!/bin/bash
+cd /home/aluno/Downloads/imagens-livros 
+for imagen in *.jpg
+do
+	echo $imagen
+		img_sem_ext=$(ls $imagen | awk -F. '{print$1}')
+		convert $imagen  $img_sem_ext.png
+
+done
+
+
+
+
+
+
